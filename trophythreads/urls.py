96ddaec20478f0974d9081b39b6ed82ff4b2b113
Samuel Marcelino Tindaon <samuelmarcelinotindaon@gmail.com>
2025-10-22 13:54:01 +0700

    tambah forms n routing

--- trophythreads/urls.py
+++ trophythreads/urls.py
@@ -20,4 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('informasi/', include('InformasiPertandingan.urls')),
+    path('forum/', include('forumApp.urls')),
 ]
